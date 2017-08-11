0c37ae79729d02222ba79c753e96c42b6fcc2c4e
Abel <sdwyub@qq.com>
2017-08-11 15:10:24 +0800

    增加apiserver的代码

---
+++ routes/appfigures.js
@@ -0,0 +1,30 @@
+
+var express = require('express');
+var router = express.Router();
+const appFigures = require("../lib/appfigures")
+
+/* GET users listing. */
+router.get('/search', function(req, res, next) {
+    appFigures.searchAppfigures(encodeURIComponent(req.query.appname),function (figures) {
+        res.json(figures)
+    })
+});
+
+router.get('/download', function(req, res, next) {
+    appFigures.requestAppfigures(req.query.appid,function (figures) {
+        res.set({
+            'Content-Type': 'application/octet-stream',
+            'Content-Disposition': 'attachment; filename='+require('urlencode')(req.query.appname)+'.csv'
+        });
+        var Readable = require('stream').Readable;
+        var s = new Readable();
+        s._read = function noop() {}; // redundant? see update below
+        s.push(figures);
+        s.push(null);
+        s.pipe(res)
+
+    })
+});
+
+
+module.exports = router;
