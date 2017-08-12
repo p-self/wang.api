3765099db0a0de06a48457af555e38f07e0875b7
Abel <sdwyub@qq.com>
2017-08-12 11:25:43 +0800

    增强程序适配性

--- routes/appfigures.js
+++ routes/appfigures.js
@@ -26,5 +26,46 @@ router.get('/download', function(req, res, next) {
     })
 });
 
+router.post('/appannie', function(req, res, next) {
+    require("request").get({
+        url: `https://www.appannie.com/apps/ios/app/${req.body.app}/details/`,
+        headers: {
+            'user-agent':'Mozilla',
+            'authority':' www.appannie.com',
+            "cookie":`${decodeURIComponent(req.body.cookie)}`,
+            'Content-Type':'text/html;charset=utf-8'
+        },
+    }, (err, response, body) => {
+        const $ = require("cheerio").load(body, {
+            xml: {
+                normalizeWhitespace: true,
+            }
+        });
+
+        let ret = [];
+
+        $(".app_content_section.app-version-block h5").each(function (i,o) {
+
+            let version = /(\d+\.\d+\.\d+)\s*\(([^\(\)]*)\)/ig.exec($(o).text());
+
+            if(version){
+                ret.push([
+                    version[1],version[2],$(o).next("div").text()
+                ])
+            }
+        });
+
+        res.set({
+            'Content-Type': 'application/octet-stream',
+            'Content-Disposition': 'attachment; filename='+require('urlencode')(req.body.app)+'.csv'
+        });
+        let Readable = require('stream').Readable;
+        let s = new Readable();
+        s._read = function noop() {}; // redundant? see update below
+        s.push(ret.join("\n"));
+        s.push(null);
+        s.pipe(res)
+    });
+});
 
 module.exports = router;
