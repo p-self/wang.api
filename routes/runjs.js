62f07a2142c6f9c9a19bed82a9f85e80c00b9ec0
Abel <sdwyub@qq.com>
2017-08-16 08:48:04 +0800

    增加runjs的接口配置
    增加完接口再增加页面，so easy

--- routes/runjs.js
+++ routes/runjs.js
@@ -16,7 +16,8 @@ const sequelize = new Sequelize(config.db, config.username, config.password, {
 const Runjs = sequelize.define('runjs', {
     css: Sequelize.STRING,
     js: Sequelize.STRING,
-    html: Sequelize.STRING
+    html: Sequelize.STRING,
+    show_code:Sequelize.STRING,//兼容原来的runjs
 });
 
 /**
@@ -26,26 +27,32 @@ router.post('/', function(req, res, next) {
     const js = req.body.js,
         css = req.body.css,
         html = req.body.html,
+        show_code = req.body.show_code,
         id = req.body.id;
     if(id){
         //更新
-        Runjs.findOne({ where: { id: id } }).then(runjs => {
-            if(css){
+        Runjs.findOne({where: {id: id}}).then(runjs => {
+            if (css) {
                 runjs.css = css;
             }
-            if(html){
+            if (html) {
                 runjs.html = html;
             }
-            if(js){
+            if (js) {
                 runjs.js = js;
+            }
+            if (show_code) {
+                runjs.show_code = show_code;
             }
 
             runjs.save();
         })
     }else{
         //新增
-        Runjs.sync({}).then(() => {
-            return  Runjs.create({
+        Runjs.sync({
+            force:true
+        }).then(() => {
+            Runjs.create({
                 js,
                 html,
                 css
@@ -70,6 +77,24 @@ router.get('/:id', function(req, res, next) {
  */
 router.get('/view/:id', function(req, res, next) {
     Runjs.findOne({ where: { id: req.params.id } }).then(runjs => {
+        const $ = require("cheerio").load(runjs.html, {
+            xml: {
+                normalizeWhitespace: true,
+            }
+        });
+
+        //在head前面追加style
+        $("head").append($(`<style>${runjs.css}</style>`))
+
+        //在body后面追加js
+        $("body").append($(`<script>${runjs.js}</script>`))
+
+        res.send($.html());
+    })
+});
+
+router.get('/show/:show_code', function(req, res, next) {
+    Runjs.findOne({ where: { show_code: req.params.show_code } }).then(runjs => {
         const $ = require("cheerio").load(runjs.html, {
             xml: {
                 normalizeWhitespace: true,
